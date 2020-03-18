6d1c745ba67fd8885219dfe0695b774e0959a45d
Dhananjana97 <sachinthadhananjana.16@cse.mrt.ac.lk>
2020-03-18 22:58:06 +0530

    initialize with spring boot

---
+++ accident_monitor/src/main/java/com/fyp/accident_monitor/AccidentMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.fyp.accident_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccidentMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccidentMonitorApplication.class, args);
+	}
+
+}
---
+++ accident_monitor/src/test/java/com/fyp/accident_monitor/AccidentMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyp.accident_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccidentMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
